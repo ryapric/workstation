3ebba5e4ed863e2f15b6915b8ea94abc74ea59c6
Ryan J. Price <ryapric@gmail.com>
2023-02-07 19:47:11 -0600

    It seems that dolphin-emu finally got deps fixed on Debian Sid

--- system/init-modules/gaming.sh
+++ system/init-modules/gaming.sh
@@ -11,8 +11,7 @@ init-gaming() {
   mkdir -p "${HOME}"/GameROMs
   # mupen64plus is plugin-based, so the glob will get any available
   sudo apt-get install -y \
-    'mupen64plus-*'
-    # TODO: broken in Sid until Qt req is bumped
-    # dolphin-emu \
-    # dolphin-emu-data \
+    'mupen64plus-*' \
+    dolphin-emu \
+    dolphin-emu-data
 }
