b33d7bdb9252703865adde6d2cd0a394b6a45b0e
Ryan J. Price <ryapric@gmail.com>
2023-03-28 15:46:32 -0500

    Whoops missed a pip-install

--- system/init-modules/python.sh
+++ system/init-modules/python.sh
@@ -5,14 +5,11 @@ init-python() {
   log-info 'Configuring Python...'
   sudo apt-get install -y \
     python3 \
+    ipython3 \
+    python3-mypy \
     python3-pip \
+    python3-pytest \
+    python3-pytest-cov \
     python3-venv \
   || log-error 'Error when installing Python utilities!'
-
-  python3 -m pip install --user --no-warn-script-location \
-    ipython \
-    mypy \
-    pytest \
-    pytest-cover \
-  || log-error 'Error when installing pip packages!'
 }
