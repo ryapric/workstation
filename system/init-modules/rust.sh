0778b464c9e40051a290859c9b39bf837ea238cb
Ryan J. Price <ryapric@gmail.com>
2023-03-28 15:43:38 -0500

    Vargant can't download rustup-init to whatever directory it's trying for, apparently

--- system/init-modules/rust.sh
+++ system/init-modules/rust.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 init-rust() {
   log-info 'Installing rust...'
-  curl -fsSL -O https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init
-  chmod +x ./rustup-init
-  ./rustup-init -y
-  rm ./rustup-init
+  curl -fsSL -o /tmp/rustup-init https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init
+  chmod +x /tmp/rustup-init
+  /tmp/rustup-init -y
+  rm /tmp/rustup-init
 }
