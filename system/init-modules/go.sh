69df107f34bc528f4aece415dd3f4f2519e74e58
Ryan J. Price <ryapric@gmail.com>
2023-10-21 20:22:01 -0500

    Add go-install for nektos/act (maybe will move later)

--- system/init-modules/go.sh
+++ system/init-modules/go.sh
@@ -8,6 +8,7 @@ init-go() {
   log-info 'Installing Go tooling...'
   pkgs=(
     golang.org/x/pkgsite/cmd/pkgsite@latest
+    github.com/nektos/act@latest
   )
 
   for pkg in "${pkgs[@]}" ; do go install "${pkg}" ; done
