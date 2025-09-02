840690b3d09b5b48da15c7476f451048256b6f35
Ryan J. Price <ryapric@gmail.com>
2025-09-02 16:48:44 -0500

    Remove fonts task, fix dotfiles script for macOS

--- dotfiles-and-config/setup.sh
+++ dotfiles-and-config/setup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Try to make grep calls more portable between Linux and macOS, ugh
+gnu_grep='grep '
+if [[ "$(uname -s)" == "Darwin" ]] ; then
+  gnu_grep='ggrep '
+fi
+
 # Using envsubst in a single call seems to only use the last line in the file, so envsubst the whole
 # thing to a separate file first
 <map.txt envsubst > /tmp/map.txt
 
 while read -r line; do
   # Skip comments and blank/malformed lines
-  if grep -qE '^#' <(echo "${line}") || grep -qE '^\s+?$' <(echo "${line}"); then continue; fi
+  if "${gnu_grep}" -qE '^#' <(echo "${line}") || "${gnu_grep}" -qE '^\s+?$' <(echo "${line}"); then continue; fi
 
   src=$(realpath "$(awk '{ print $1 }' <<< "${line}")")
   tgt=$(awk '{ print $2 }' <<< "${line}")
